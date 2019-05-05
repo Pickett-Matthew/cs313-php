ca5d4c2e7cf77c2c551d33c23a09b9eed7065665
matthew pickett <pic11005@byui.edu>
2019-05-04 18:29:54 -0600

    added Homepage

---
+++ web/homepage/homepage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Matt Pickett CS-313</h1>
+        <nav class="nav-bar">
+            <ul id="nav-items">
+                <li>Matt Pickett</li>
+                <li>S.E. Major</li>
+                <li>Rigby, ID</li>
+                <li id="user"></li>
+            </ul>
+        </nav>
+        <div id="center_info">
+            <p id="about_me"><img src="matt_family.jpg">Hey everyone, my name is Matt Pickett. I'm a senior and I can't wait to graduate. My dream is to work as a software developer and live where there is lots of space and land to play on. I grew up in Peoria, AZ and lived about 5 min from the Phoenix temple.</p>
+        </div>
+        <div class="assignments">
+            <p id="num">Here is a link to my assignments... 404</p>
+        </div>
+    </div>
+    <?php echo "hello this is php!"; ?>
+</body></html>
