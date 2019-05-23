32c2e5a781b18463c1cd2eea29dfa2d12e96e0e0
matthew pickett <pic11005@byui.edu>
2019-05-23 15:37:15 -0600

    adding db info

--- web/show_db.php
+++ web/show_db.php
@@ -11,15 +11,9 @@
 <body>
     <h1>I am pulling info from my Database</h1>
     <?php 
-        $statement = $db->prepare("SELECT workerId, description, dueBy FROM chore");
-        $statement->execute();
-
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        foreach($db->query('SELECT childname, age FROM child') as $row) 
         {
-            $workerId = $row['workerId'];
-            $description = $row['description'];
-            $dueBy = $row['dueBy'];
-            echo "<p><strong>$workerId $description $dueBy </strong><p>";
+            echo $row['childname'] . " is " . $row['age'] . " years old.";
         }
     ?>
 </body>
