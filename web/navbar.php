ab4e06628d8c9e270840e0ea5c9fa681335e57fa
matthew pickett <pic11005@byui.edu>
2019-05-23 20:10:11 -0600

    add

---
+++ web/navbar.php
@@ -0,0 +1,33 @@
+<?php require 'connect.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Virtual Piggy Bank</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="show_db.php">Database</a></li>
+    </ul>
+    <form class="navbar-form navbar-left" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="piggy-banks" name="search">
+      </div>
+      <button type="submit" class="btn btn-info">Submit</button>
+    </form>
+  </div>
+</nav>
+</body>
+</html>
--- web/show_db.php
+++ web/show_db.php
@@ -1,5 +1,5 @@
 <?php require 'connect.php' ?>
-
+<?php include 'navbar.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,19 +12,21 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <h1>Welcome to your virtual piggy bank!</h1>
-        <p>Click below to see the assigned chores for your children</p>
-        <button type="submit" name="chores" class="btn btn-info">Show Chores</button><br>
-        <?php if($_SERVER['REQUEST_METHOD'] == "POST") {
-            if(isset($_POST['chores'])) {
-                foreach($db->query('SELECT chore.description, child.childname from chore inner Join 
-                child on chore.workerId = child.id') as $chore) {
-                    echo "<h4> {$chore['description']} - {$chore['childname']} </h4></br>";
+    <div class="container">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <h1>Welcome to your virtual piggy bank!</h1>
+            <p>Click below to see the assigned chores for your children</p>
+            <button type="submit" name="chores" class="btn btn-info">Show Chores</button><br>
+            <?php if($_SERVER['REQUEST_METHOD'] == "POST") {
+                if(isset($_POST['chores'])) {
+                    foreach($db->query('SELECT chore.description, child.childname from chore inner Join 
+                    child on chore.workerId = child.id') as $chore) {
+                        echo "<h4> {$chore['description']} - {$chore['childname']} </h4></br>";
+                    }
                 }
             }
-        }
-        ?>
-    </form>
+            ?>
+        </form>
+    </div>
 </body>
 </html>
