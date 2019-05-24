0bbe30518ffc1ef202dc56859683e95e6de650be
matthew pickett <pic11005@byui.edu>
2019-05-23 20:00:43 -0600

    add

--- web/show_db.php
+++ web/show_db.php
@@ -3,16 +3,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Document</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <h1>Welcome to your virtual piggy bank!</h1>
         <p>Click below to see the assigned chores for your children</p>
-        <button type="submit" name="chores">Show Chores</button><br>
+        <button type="submit" name="chores" class="btn btn-info">Show Chores</button><br>
         <?php if($_SERVER['REQUEST_METHOD'] == "POST") {
             if(isset($_POST['chores'])) {
                 foreach($db->query('SELECT chore.description, child.childname from chore inner Join 
