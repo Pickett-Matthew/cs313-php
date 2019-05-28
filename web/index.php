4e3018941d0f2333058a1fd4decd233f5427126d
matthew pickett <pic11005@byui.edu>
2019-05-28 14:57:39 -0600

    add files

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<?php include '/week05/connect.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class='container'>
+        <form action="">
+            <p>Insert a New Scripture</p>
+            <input type="text" name="book"><br>
+            <input type="text" name="chapter"><br>
+            <input type="text" name="verse"><br>
+            <input type="textarea" name="content" rows=3><br>
+            
+            <?php 
+                foreach($db->query('SELECT id, name FROM topic') as $row) {
+                    echo $row['id'];
+                    echo $row['name'];
+                }
+            ?>
+        </form>
+    </div>
+</body>
+</html>
