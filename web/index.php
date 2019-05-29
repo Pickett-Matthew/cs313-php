50aa6ec3de088bcaddd63978334ea35df310ecd9
matthew pickett <pic11005@byui.edu>
2019-05-28 18:24:31 -0600

    added files

---
+++ web/index.php
@@ -0,0 +1,30 @@
+<?php include 'db.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class='container'>
+        <form action="">
+            <p>Insert a New Scripture</p>
+            <input type="text" name="book"><br>
+            <input type="text" name="chapter"><br>
+            <input type="text" name="verse"><br>
+            <input type="textarea" name="content" rows=3><br>
+            
+            <?php 
+                foreach($db->query('SELECT id, name FROM topic') as $row) {
+                    $id = $row['id'];
+                    echo "<input type='checkbox' name='boxes[]' id=$id>";
+                    echo $row['name'];
+                }
+            ?>
+        </form>
+    </div>
+</body>
+</html>
