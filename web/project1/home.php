eca95f0d3dec342e0831d82bb9b45cf9a4175000
matthew pickett <pic11005@byui.edu>
2019-06-06 13:52:48 -0600

    added createAcct, login, signup

--- web/project1/home.php
+++ web/project1/home.php
@@ -58,6 +58,22 @@ else
         </div>
     </form>
 
+    <div class="container">
+        <form action="$_SERVER['PHP_SELF']" method='POST'>
+            <select name="cars" class="custom-select">
+                <option selected>Custom Select Menu</option>
+                <option value="volvo">Volvo</option>
+                <option value="fiat">Fiat</option>
+                <option value="audi">Audi</option>
+            </select>
+        </form>
+
+        <?php 
+            $option = $_POST['cars'];
+            echo $option;
+        ?>
+
+    </div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
