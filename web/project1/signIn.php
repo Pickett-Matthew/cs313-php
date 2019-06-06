75deb2767da68a943ded8d8015a82757484f1c27
matthew pickett <pic11005@byui.edu>
2019-06-06 01:09:36 -0600

    added createAcct, login, signup

--- web/project1/signIn.php
+++ web/project1/signIn.php
@@ -62,21 +62,23 @@ if($badLogin)
             Virtual Piggy Bank
         </a>
     </nav>
-    <div class="container">
-        <form action="signIn.php" method="POST">
-            <div class="form-row align-items-center">
-                <div class="col-auto">
-                    <input class="form-control mb-2" type="text" name="txtName" id="txtName" Placeholder="Username">
-                </div>
+    <div class="login-form">
+        <div class="row justify-content-center">
+            <form action="signIn.php" method="POST">
+                <div class="form-row align-items-center">
+                    <div class="col-auto">
+                        <input class="form-control mb-2" type="text" name="txtName" id="txtName" Placeholder="Username">
+                    </div>
 
-                <div class="col-auto">
-                    <input class="form-control mb-2" type="password" id="txtPassword" name="txtPassword" Placeholder="password">
+                    <div class="col-auto">
+                        <input class="form-control mb-2" type="password" id="txtPassword" name="txtPassword" Placeholder="password">
+                    </div>
+                    <button class="btn btn-primary" type="submit">Sign In</button>
                 </div>
-                <button class="btn btn-primary" type="submit">Sign In</button>
-            </div>
-        </form><br>
-        <h6>Don't have an account?<button class="btn btn-outline-primary btn-sm"><a href="SignUp.php">Sign Up</a></button></h6>
+            </form>
+        </div>
     </div>
+    <h6>Don't have an account?<button class="btn btn-outline-primary btn-sm"><a href="SignUp.php">Sign Up</a></button></h6>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
