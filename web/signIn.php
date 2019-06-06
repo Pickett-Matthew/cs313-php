aa6203e9095a2fd567e0f2fb81e5ca5d0084e1ef
matthew pickett <pic11005@byui.edu>
2019-06-06 00:48:04 -0600

    added createAcct, login, signup

--- web/signIn.php
+++ web/signIn.php
@@ -46,15 +46,16 @@ if(isset($_POST['txtName']) && isset($_POST['txtPassword']))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link res="stylesheet" href="myStyle.css">
     <title>Sign In</title>
 </head>
-<body>
 <?php 
 if($badLogin)
 {
     echo "Incorrect username or password!<br>";
 }
 ?>
+<body>
     <div class="container">
         <form action="signIn.php" method="POST">
             <div class="form-row align-items-center">
