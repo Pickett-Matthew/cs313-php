2f50fdf31ef5a8fa76320ddf660c9bd5df2ed9b0
matthew pickett <pic11005@byui.edu>
2019-06-06 13:54:38 -0600

    added createAcct, login, signup

--- web/project1/home.php
+++ web/project1/home.php
@@ -72,7 +72,7 @@ else
             $option = $_POST['cars'];
             echo $option;
         ?>
-
+        <p><?php $option ?></p>
     </div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
