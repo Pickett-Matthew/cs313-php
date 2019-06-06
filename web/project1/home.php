f6f1de1dab0613090b62829f1ff8dfb0f7828106
matthew pickett <pic11005@byui.edu>
2019-06-06 02:37:54 -0600

    added createAcct, login, signup

--- web/project1/home.php
+++ web/project1/home.php
@@ -50,13 +50,13 @@ else
                 }
             ?>
         </select>
+        <div class="container">
+            <?php
+                $option = $_POST['option'];
+                echo $option; 
+            ?>
+        </div>
     </form>
-
-    <?php
-    $option = $_POST['option'];
-    echo $option;
-        
-    ?>
 
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
