af866881e3fa7f530576a6962354110e51b65a54
matthew pickett <pic11005@byui.edu>
2019-06-06 20:46:44 -0600

    added createAcct, login, signup

--- web/project1/SignUp.php
+++ web/project1/SignUp.php
@@ -6,15 +6,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Sign Up</title>
 </head>
 <body>
-    <form action="createAcct.php" method="POST">
-        Name: <input type="text" name="txtName" id="txtName"><br>
-        Password: <input type="password" name="txtPassword" id="txtPassword"><br>
+    <div class="container ">
+        <form action="createAcct.php" method="POST">
+            <div class="form-group">
+                <label for="txtName" class="form-group">Name</label>
+                <input type="text" class="form-control col-sm-4" name="txtName" id="txtName"><br>
 
-        <input type="submit" value="Create Account">
-        
-    </form>
+                <label for="password" class="form-group">Password</label>
+                <input type="password" class="form-control col-sm-4" name="txtPassword" id="txtPassword"><br>
+
+            <input type="submit" class="btn btn-info" value="Create Account">
+            </div>
+        </form>
+    </div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
