b8009b44d49ae3eac5bc32f5c8a5313b6467dc10
matthew pickett <pic11005@byui.edu>
2019-06-06 19:38:04 -0600

    added createAcct, login, signup

--- web/project1/addChore.php
+++ web/project1/addChore.php
@@ -6,13 +6,14 @@ $child = $_POST['name'];
 $description = $_POST['description'];
 $c_value = $_POST['chore-value'];
 
-
-$query = "SELECT id from child where childname = $child";
-$workerid = $db->prepare($query);
-
 echo $child . "<br>";
 echo $$description . "<br>";
 echo $c_value . "<br>";
+
+
+
+$query = "SELECT id from child where childname = $child";
+$workerid = $db->prepare($query);
 echo $workerid . "<br>";
 
 
